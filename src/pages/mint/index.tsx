9840eff20443b1ae029964da74fdef8dcff87a47
dilrong <dilrong@dilrong.com>
2023-01-15 20:42:32 +0900

    feat: activate

--- src/pages/mint/index.tsx
+++ src/pages/mint/index.tsx
@@ -70,9 +70,6 @@ export default function Home() {
 
   useDidMountEffect(() => {
     activate(injectedConnector);
-  }, []);
-
-  useDidMountEffect(() => {
     if (chainId !== 1 && chainId !== undefined) {
       toast.error(t("mint-desc4"));
     }
